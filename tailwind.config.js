04b06a7f7305556755de6694a86dc3443fe0206c
CarloshDevBR <carlos.sortudox1@gmail.com>
2024-01-10 17:38:30 -0300

    chore: cores adicionadas no backgroundColor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./App.tsx', './src/screens/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'primary-green': '#31CF67',
+        'primary-red': '#E23C44',
+      },
+    },
   },
   plugins: [],
 };
